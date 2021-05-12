4fc95809ad9363870f336fcf692828fafe80f351
YK <yk@example.com>
2021-05-12 12:17:49 +0200

    Added getting-started container

--- src/test/java/mytests/SearchTest.java
+++ src/test/java/mytests/SearchTest.java
@@ -8,7 +8,13 @@ import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.testcontainers.containers.BrowserWebDriverContainer;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.output.Slf4jLogConsumer;
+import org.testcontainers.containers.wait.strategy.Wait;
+import org.testcontainers.utility.DockerImageName;
 
 import java.io.File;
 
@@ -16,11 +22,17 @@ import static com.codeborne.selenide.CollectionCondition.sizeGreaterThan;
 import static com.codeborne.selenide.Selenide.*;
 
 public class SearchTest {
+  private final static Logger LOG = LoggerFactory.getLogger(SearchTest.class);
   @Rule
   public BrowserWebDriverContainer browser =
       new BrowserWebDriverContainer()
           .withRecordingMode(BrowserWebDriverContainer.VncRecordingMode.RECORD_ALL, new File("target"))
           .withCapabilities(DesiredCapabilities.firefox()); // DesiredCapabilities.chrome()
+  @Rule
+  public GenericContainer app = new GenericContainer(DockerImageName.parse("getting-started"))
+          .withExposedPorts(3000)
+          .withLogConsumer(new Slf4jLogConsumer(LOG))
+          .waitingFor(Wait.forLogMessage(".*Listening on port .*\\n", 1));
 
   @Before
   public void setUp() {
@@ -36,6 +48,7 @@ public class SearchTest {
 
   @Test
   public void search() {
+    System.out.println("App in container: " + app.getHost() + ":" + app.getFirstMappedPort());
     open("https://duckduckgo.com/");
     $(By.name("q")).val("codeborne").pressEnter();
     $$(".results .result").shouldHave(sizeGreaterThan(5));
