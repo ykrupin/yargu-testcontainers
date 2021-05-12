368d741a779bd71817f8f92328526cd3f3c1131e
krupin <yu.krupin@krista.ru>
2021-05-12 10:42:59 +0300

    Minimal sample

---
+++ src/test/java/mytests/SearchTest.java
@@ -0,0 +1,43 @@
+package mytests;
+
+import com.codeborne.selenide.WebDriverRunner;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testcontainers.containers.BrowserWebDriverContainer;
+
+import java.io.File;
+
+import static com.codeborne.selenide.CollectionCondition.sizeGreaterThan;
+import static com.codeborne.selenide.Selenide.*;
+
+public class SearchTest {
+  @Rule
+  public BrowserWebDriverContainer browser =
+      new BrowserWebDriverContainer()
+          .withRecordingMode(BrowserWebDriverContainer.VncRecordingMode.RECORD_ALL, new File("target"))
+          .withCapabilities(DesiredCapabilities.firefox()); // DesiredCapabilities.chrome()
+
+  @Before
+  public void setUp() {
+    RemoteWebDriver driver = browser.getWebDriver();
+    System.out.println(browser.getVncAddress());
+    WebDriverRunner.setWebDriver(driver);
+  }
+
+  @After
+  public void tearDown() {
+    WebDriverRunner.closeWebDriver();
+  }
+
+  @Test
+  public void search() {
+    open("https://duckduckgo.com/");
+    $(By.name("q")).val("codeborne").pressEnter();
+    $$(".results .result").shouldHave(sizeGreaterThan(5));
+  }
+}
